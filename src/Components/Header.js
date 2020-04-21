ed28b8275286c3bf184c9665264ba569b9398ffe
aliwo <aliwo@naver.com>
2020-04-22 00:12:00 +0900

    메인 타이틀 설명 부분  수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,12 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     if(this.props.data){
-        var phrase = this.props.data.phrase;
+        var title = this.props.data.title;
         var name = this.props.data.name;
         var occupation = this.props.data.occupation;
-        var description = this.props.data.description;
+        var description1 = this.props.data.description1;
+        var descriptionHighlight = this.props.data.descriptionHighlight;
+        var description2 = this.props.data.description2;
         var city = this.props.data.address.city;
         var networks = this.props.data.social.map(function(network){
           return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
@@ -34,8 +36,8 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline"> {phrase} </h1>
-            <h3>I'm a {city} based <span>{occupation} </span> {description}</h3>
+            <h1 className="responsive-headline"> {title} </h1>
+            <h3> {description1} <span>{descriptionHighlight} </span> {description2}</h3>
             <hr />
             <ul className="social">
           {networks}
