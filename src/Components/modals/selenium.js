c11df6f88a901106e9bb8e69b5fd420b06f064de
aliwo <aliwo@naver.com>
2020-05-10 19:30:45 +0900

    콘솔이 왱왱 거리는거 들어주기

--- src/Components/modals/selenium.js
+++ src/Components/modals/selenium.js
@@ -18,11 +18,11 @@ class Selenium extends Component {
             <path d="M481.587,3292.048l.03-.02-16.1-24.589-2.8,1.831,1.878,2.868H359.868l13.041,19.956h0l-13.041,19.956H464.652l-1.786,2.8,2.819,1.8,4.45-6.987,11.481-17.568Z" transform="translate(0 -3076.441)" fill="#535b95"/>
             <path d="M599.336,3292.048l.03-.02-16.1-24.589-2.8,1.831,1.878,2.868H477.617l13.041,19.956h0l-13.041,19.956H582.4l-1.786,2.8,2.819,1.8,4.45-6.987,11.481-17.568Z" transform="translate(0 -3076.441)" fill="#0088ce"/>
             <path d="M717.085,3292.048l.03-.02-16.1-24.589-2.8,1.831,1.878,2.868H595.366l13.041,19.956h0l-13.041,19.956H700.15l-1.786,2.8,2.819,1.8,4.45-6.987,11.481-17.568Z" transform="translate(0 -3076.441)" fill="#1a9887"/>
-            <text transform="translate(152.065 218.94)" fill="#fff" font-size="12" font-family="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">셀레니움 실행</tspan></text>
-            <text transform="translate(285.237 218.94)" fill="#fff" font-size="12" font-family="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">로그인</tspan></text>
-            <text transform="translate(512.221 218.94)" fill="#fff" font-size="12" font-family="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">상품 선택</tspan></text>
-            <text transform="translate(396.648 218.941)" fill="#fff" font-size="12" font-family="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">포인트 사용</tspan></text>
-            <text transform="translate(631.224 218.941)" fill="#fff" font-size="12" font-family="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">결제 완료!</tspan></text>
+            <text transform="translate(152.065 218.94)" fill="#fff" fontSize="12" fontFamily="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">셀레니움 실행</tspan></text>
+            <text transform="translate(285.237 218.94)" fill="#fff" fontSize="12" fontFamily="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">로그인</tspan></text>
+            <text transform="translate(512.221 218.94)" fill="#fff" fontSize="12" fontFamily="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">상품 선택</tspan></text>
+            <text transform="translate(396.648 218.941)" fill="#fff" fontSize="12" fontFamily="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">포인트 사용</tspan></text>
+            <text transform="translate(631.224 218.941)" fill="#fff" fontSize="12" fontFamily="NanumBarunGothicBold, NanumBarunGothic"><tspan x="0" y="0">결제 완료!</tspan></text>
           </g>
         </svg>
 
